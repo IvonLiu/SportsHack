ac308efb6f2221fb3419c3a3172cfa409e783e4e
Alex <alexonej@gmail.com>
2015-11-28 02:42:28 -0800

    center align on setup page issue

--- js/init.js
+++ js/init.js
@@ -24,14 +24,10 @@ var theater = theaterJS()
 //    })
 theater
     .on('type:start, erase:start', function () {
-      // add a class to actor's dom element when he starts typing/erasing
-      var actor = theater.getCurrentActor()
-      actor.$element.classList.add('is-typing')
+        theater.getCurrentActor().$element.classList.add('actor__content--typing')
     })
     .on('type:end, erase:end', function () {
-      // and then remove it when he's done
-      var actor = theater.getCurrentActor()
-      actor.$element.classList.remove('is-typing')
+        theater.getCurrentActor().$element.classList.remove('actor__content--typing')
     })
 
 theater
